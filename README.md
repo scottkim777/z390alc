97305f67d35ebc9ed6ad2b6b656a9b2981e8d720
Scott Kim <scottjkim777@gmail.com>
2018-12-18 08:34:32 -0500

    latest

---
+++ README.md
@@ -0,0 +1,11 @@
+# Support for Z390 ALC in Visual Studio Code
+
+**Z390 ALC** Extension for Visual Studio Code provides limited features for coding ALC (Assembly Language) for Z390 assembler.  Use the z390 assembler emulator from (http://www.z390.org/) and install the z390 assembler.
+
+**Instruction** Obviously not all instruction sets are included for the z390, but only ones that were relevant for the Assembler class, which are substantial.  Refer to IBM's Principals of Operation for zSeries for more information and detail.  
+
+**Disclaimer** This extension was created for 'learning purposes' only and will not be published to the market place.  So please keep this in mind.  Based on z80 Assembler extension created by Imanolea.   
+
+**LICENSING** 
+The Z80 Assembly extension is subject to these license terms.  
+
--- out/src/extension.js
+++ out/src/extension.js
@@ -7,15 +7,29 @@ const completionProposer_1 = require("./completionProposer");
 const definitionProvider_1 = require("./definitionProvider");
 const symbolProvider_1 = require("./symbolProvider");
 let symbolDocumenter;
+
+async function asmlg() {
+    vscode.commands.executeCommand('vscode.open', vscode.Uri.parse('http://www-01.ibm.com/support/docview.wss?uid=isg2b9de5f05a9d57819852571c500428f9a'));
+}
 function activate(ctx) {
     const languageSelector = { language: "z390-alc", scheme: "file" };
     symbolDocumenter = new symbolDocumenter_1.ALCSymbolDocumenter();
+
+    let disp = vscode.commands.registerCommand(
+        "extension.asmlg",
+        function() {
+          asmlg();
+        }
+      );
+
+    ctx.subscriptions.push(disp)
     ctx.subscriptions.push(vscode.languages.registerHoverProvider(languageSelector, new hover_1.ALCHoverProvider(symbolDocumenter)));
     ctx.subscriptions.push(vscode.languages.registerCompletionItemProvider(languageSelector, new completionProposer_1.ALCCompletionProposer(symbolDocumenter), ','));
     ctx.subscriptions.push(vscode.languages.registerDefinitionProvider(languageSelector, new definitionProvider_1.ALCDefinitionProvider(symbolDocumenter)));
     ctx.subscriptions.push(vscode.languages.registerDocumentSymbolProvider(languageSelector, new symbolProvider_1.ALCDocumentSymbolProvider(symbolDocumenter)));
     ctx.subscriptions.push(vscode.languages.registerWorkspaceSymbolProvider(new symbolProvider_1.ALCWorkspaceSymbolProvider(symbolDocumenter)));
 }
+
 exports.activate = activate;
 function deactivate() {
     if (symbolDocumenter) {
